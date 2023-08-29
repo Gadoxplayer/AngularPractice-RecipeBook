9d8cb494f569913d40b117e9315153a3475d0e87
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2023-08-29 17:59:11 -0500

    updating routing first steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: 'item', component: RecipeItemComponent, children: [
+      { path: 'detail', component: RecipeDetailComponent }
+    ] }
+  ]},
+  { path: 'shopping-list', component: ShoppingListComponent },
+  //{ path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
