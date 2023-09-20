0f0ae1f86356763a6a56240e7d73cd666f1fe866
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2023-09-20 17:48:39 -0500

    adding route for recipe edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-i
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
-    { path: ':id', component: RecipeDetailComponent }
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },    
+    { path: ':id/edit', component: RecipeEditComponent }
   ]},
   { path: 'shopping-list', component: ShoppingListComponent },
   //{ path: '**', redirectTo: '/'}
