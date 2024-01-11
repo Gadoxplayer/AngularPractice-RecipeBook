64d908da621f792855875edec9473a628442d725
EduardoCampo <eduardocampo91@gmail.com>
2024-01-10 22:56:16 -0500

    finishing updating the fomr using td, giving functionality to the buttons in the shoiping list component

--- src/app/shopping-list/shooping-edit/shooping-edit.component.ts
+++ src/app/shopping-list/shooping-edit/shooping-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient-model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
@@ -7,10 +13,10 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-shooping-edit',
   templateUrl: './shooping-edit.component.html',
-  styleUrls: ['./shooping-edit.component.css']
+  styleUrls: ['./shooping-edit.component.css'],
 })
 export class ShoopingEditComponent implements OnInit, OnDestroy {
-  @ViewChild('f', {static: false}) slForm: NgForm;
+  @ViewChild('f', { static: false }) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
@@ -19,19 +25,20 @@ export class ShoopingEditComponent implements OnInit, OnDestroy {
   // @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
   // @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
-    this.subscription = this.slService.startedEditing
-      .subscribe((index: number)=>{
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
         this.editedItem = this.slService.getIngredient(index);
         this.slForm.setValue({
-          name:this.editedItem.name, 
-          amount: this.editedItem.amount
-        })
-      });
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        });
+      }
+    );
   }
 
   onSetIngredients(form: NgForm) {
@@ -39,10 +46,26 @@ export class ShoopingEditComponent implements OnInit, OnDestroy {
     // const ingreAmount = this.amountInputRef.nativeElement.value;
     const value = form.value;
     const newIgredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIgredient);
-  }
-  ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIgredient);
+    } else {
+      this.slService.addIngredient(newIgredient);
+    }
+    this.editMode = false;
+    form.reset();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onClearItems() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(form: NgForm) {
+      this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClearItems();
+  }
 }
