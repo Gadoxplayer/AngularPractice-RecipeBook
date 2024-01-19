973e3650c150298daa2f726d8e52247fb3c2a538
EduardoCampo <eduardocampo91@gmail.com>
2024-01-19 00:48:50 -0500

    giving functionality to the delete recipe button, making the save and cancel button navigate away to the proper page

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
 
@@ -16,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -27,6 +28,10 @@ export class RecipeEditComponent implements OnInit {
     });
   }
   //ractive aproach
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
 
   onSubmit() {
     console.log(this.recipeForm);
@@ -42,6 +47,7 @@ export class RecipeEditComponent implements OnInit {
       this.recipeService.addRecipe(newRecipe);
     }
     this.editMode = false;
+    this.onCancel();
   }
 
   private initForm() {
