fdcacfe923961b8046cf9b5f606e6576a683dcd6
EduardoCampo <eduardocampo91@gmail.com>
2024-03-01 01:02:49 -0500

    stroing recipes in a server, adding functiuonality to header buttons to save and fetch recipes in and from the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { RecipeService } from './recipes/recipe.service';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [ShoppingListService, RecipeService],
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-practice-ac176-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-practice-ac176-default-rtdb.firebaseio.com/recipes.json')
+        .subscribe((recipes) => {
+            console.log(recipes);
+            this.recipeService.setRecipes(recipes);
+        })
+
+  }
+}
